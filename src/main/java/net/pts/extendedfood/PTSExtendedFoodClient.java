5c0f31e374aba75f19bdea8267e40ebb0ba82b78
ptskalman <joel.abrahamsson@elev.ga.ntig.se>
2024-01-25 08:13:49 +0100

    Initial commit

---
+++ src/main/java/net/pts/extendedfood/PTSExtendedFoodClient.java
@@ -0,0 +1,10 @@
+package net.pts.extendedfood;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PTSExtendedFoodClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
